f79291ffe6317102e3cb58003d2109e2564c5215
BrandonSaravia <BPSaravia@gmail.com>
2019-02-28 16:23:56 -0500

    built instuctor and student class as children of base class Person and added ProjectManagers class as child of instructor

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -12,7 +12,60 @@ class Person{
     }
 }
 
+class Instructor extends Person{
+    constructor(qualities){
+        super(qualities);
+        this.specialty = qualities.specialty
+        this.favLanguage = qualities.favLanguage
+        this.catchPhrase = qualities.catchPhrase
+    }
+    demo(subject) {
+        console.log(`Today we are learning about ${subject}.`)
+    }
+    grade(student) {
+        console.log(`${student.name} receives a perfect score on ${subject}.`)
+    }
+}
 
+
+
+class Student extends Person{
+    constructor(about){
+        super(about);
+        this.previousBackground = about.previousBackground
+        this.className = about.className
+        this.favSubjects = about.favSubjects
+    }
+    listsSubjects() {
+        console.log(this.favSubjects)
+    }
+    PRAssignment(subject) {
+        console.log(`${this.name} has submitted a PR for ${subject}.`)
+    }
+    sprintChallenge(subject) {
+        console.log(`${this.name} has begun sprint challenge on ${subject}.`)
+    }
+}
+
+
+
+class ProjectManagers extends Instructor{
+    constructor(info){
+        super(about);
+        this.previousBackground = about.previousBackground
+        this.className = about.className
+        this.favSubjects = about.favSubjects
+    }
+    listsSubjects() {
+        console.log(this.favSubjects)
+    }
+    PRAssignment(subject) {
+        console.log(`${this.name} has submitted a PR for ${subject}.`)
+    }
+    sprintChallenge(subject) {
+        console.log(`${this.name} has begun sprint challenge on ${subject}.`)
+    }
+}
 
 
 
