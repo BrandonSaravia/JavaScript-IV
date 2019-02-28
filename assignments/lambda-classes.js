73bb9789dff163b264d2f948b84156aac94c7549
BrandonSaravia <BPSaravia@gmail.com>
2019-02-28 18:41:30 -0500

    Added new entries into classes making objects and loged them out

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,5 +1,14 @@
 // CODE here for your Lambda Classes
 
+
+
+
+//////////////////// Classes
+
+
+
+// base-class Person
+
 class Person{
     constructor(attributes){
         this.name = attributes.name;
@@ -12,58 +21,62 @@ class Person{
     }
 }
 
+
+
+// child-class Instructor
+
 class Instructor extends Person{
     constructor(qualities){
         super(qualities);
-        this.specialty = qualities.specialty
-        this.favLanguage = qualities.favLanguage
-        this.catchPhrase = qualities.catchPhrase
+        this.specialty = qualities.specialty;
+        this.favLanguage = qualities.favLanguage;
+        this.catchPhrase = qualities.catchPhrase;
     }
     demo(subject) {
-        console.log(`Today we are learning about ${subject}.`)
+        return `Today we are learning about ${subject}.`
     }
-    grade(student) {
-        console.log(`${student.name} receives a perfect score on ${subject}.`)
+    grade(student, subject) {
+        return `${student.name} receives a perfect score on ${subject}.`
     }
 }
 
 
+
+// child-class Student
 
 class Student extends Person{
     constructor(about){
         super(about);
-        this.previousBackground = about.previousBackground
-        this.className = about.className
-        this.favSubjects = about.favSubjects
+        this.previousBackground = about.previousBackground;
+        this.className = about.className;
+        this.favSubjects = about.favSubjects;
     }
     listsSubjects() {
-        console.log(this.favSubjects)
+        return this.favSubjects
     }
     PRAssignment(subject) {
-        console.log(`${this.name} has submitted a PR for ${subject}.`)
+        return `${this.name} has submitted a PR for ${subject}.`
     }
     sprintChallenge(subject) {
-        console.log(`${this.name} has begun sprint challenge on ${subject}.`)
+        return `${this.name} has begun sprint challenge on ${subject}.`
     }
 }
 
 
+
+// grandchild-class PMs
 
 class ProjectManagers extends Instructor{
     constructor(info){
-        super(about);
-        this.previousBackground = about.previousBackground
-        this.className = about.className
-        this.favSubjects = about.favSubjects
+        super(info);
+        this.gradClassName = info.gradClassName;
+        this.favInstructor = info.favInstructor;
     }
-    listsSubjects() {
-        console.log(this.favSubjects)
+    standUp(slackChannel) {
+        return `${this.name} announces to ${slackChannel}, @channel standy times!​​​​​`
     }
-    PRAssignment(subject) {
-        console.log(`${this.name} has submitted a PR for ${subject}.`)
-    }
-    sprintChallenge(subject) {
-        console.log(`${this.name} has begun sprint challenge on ${subject}.`)
+    debugsCode(studentObject, subject) {
+        return `${this.name} debugs ${studentObject.name}'s code on ${subject}`
     }
 }
 
@@ -71,28 +84,168 @@ class ProjectManagers extends Instructor{
 
 
 
+///////////////// User Entries
+
+
+
+// Instructors
+
+
+const josh = new Instructor({
+    name: 'Josh Knell',
+    location: 'California',
+    age: 29,
+    gender: 'male',
+    favLanguage: 'Python',
+    specialty: 'Back-end',
+    catchPhrase: `Don't forget the homies?`,
+  });
+
+
+
+  const eminem = new Instructor({
+    name: 'Marshall Bruce Mathers III',
+    location: 'California',
+    age: 47,
+    gender: 'male',
+    favLanguage: 'CSS',
+    specialty: 'Front-end',
+    catchPhrase: `Everyone is from the 313.`,
+  });
+
+
+
+  const timmy = new Instructor({
+    name: 'Timmy Turner',
+    location: 'Dimsdale',
+    age: 11,
+    gender: 'male',
+    favLanguage: 'Ruby',
+    specialty: 'Back-end',
+    catchPhrase: `What could possibly go wrong!`,
+  });
+
+
+
+
+// Students
+
+
+const brandon = new Student({
+    name: 'Brandon Saravia',
+    location: 'South Carolina',
+    age: 20,
+    gender: 'male',
+    previousBackground: 'Welder',
+    className: 'Web 18',
+    favSubjects: ['Html', 'CSS', 'JavaScript'],
+  });
+
+
+
+
+  const george = new Student({
+    name: 'George Washington',
+    location: 'Virginia',
+    age: 287,
+    gender: 'male',
+    previousBackground: 'U.S. President',
+    className: 'Web 18',
+    favSubjects: ['Html', 'CSS', 'JavaScript'],
+  });
+
+
+
+
+  const luke = new Student({
+    name: 'Luke Skywalker',
+    location: 'A Gallaxy Far Far Away',
+    age: 19,
+    gender: 'male',
+    previousBackground: 'Robot Mechanic',
+    className: 'Web 18',
+    favSubjects: ['Html', 'CSS', 'JavaScript'],
+  });
 
 
 
 
 
+// Project Managers
+
+
+const dennis = new ProjectManagers({
+    name: 'Dennis Orbson',
+    location: 'Nomad',
+    age: 25,
+    gender: 'male',
+    gradClassName: 'Web 13',
+    favInstructor: 'Marshall Bruce Mathers III',
+    specialty: 'redux',
+    favLanguage: 'python',
+    catchPhrase: 'thats cool',
+});
 
 
 
 
+const matt = new ProjectManagers({
+    name: 'Matt Damon',
+    location: 'Hollywood',
+    age: 46,
+    gender: 'male',
+    gradClassName: 'Web 1000',
+    favInstructor: 'Josh Knell',
+    specialty: 'redux',
+    favLanguage: 'C#',
+    catchPhrase: 'Im Matt freaking Damon!',
+});
 
 
 
 
+const vader = new ProjectManagers({
+    name: 'Darth Vader',
+    location: 'A Gallaxy Far Far Away',
+    age: 40,
+    gender: 'male',
+    gradClassName: 'Web 3',
+    favInstructor: 'Timmy Turner',
+    specialty: 'redux',
+    favLanguage: 'python',
+    catchPhrase: 'Phew Chhhh',
+});
 
 
 
 
+///////////////// console logs
+
+
+// Instructor
+console.log(josh);
+console.log(josh.speak());
+console.log(josh.demo('Javascript'));
+console.log(josh.grade(brandon,'lambda-classes.js'));
 
 
 
+// Student
+console.log(brandon);
+console.log(brandon.speak());
+console.log(brandon.listsSubjects());
+console.log(brandon.PRAssignment('Javascript IV'));
+console.log(brandon.sprintChallenge('Feb 29, 2019'));
 
 
+
+// PMs
+console.log(dennis);
+console.log(dennis.speak());
+console.log(dennis.demo('Javascript'));
+console.log(dennis.grade(brandon,'lambda-classes.js'));
+console.log(dennis.standUp('web18_dennis'));
+console.log(dennis.debugsCode(brandon,'lambda-classes.js'));
 
 
 
